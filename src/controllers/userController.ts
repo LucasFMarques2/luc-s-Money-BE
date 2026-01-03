153d2ba8bc3e0cbdc26c9688b1ca52cd6e67778a
lucasfmarques2 <lucasfreitasm3@hotmail.com>
2026-01-03 14:46:06 -0300

    Add rota user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,23 +9,23 @@ const userSchema = z.object({
   password: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
 })
 
+
 export const userController = {
   async create(req: Request, res: Response) {
     try {
-
-
       const data = userSchema.parse(req.body)
       const newUser = await userService.createUser(data)
-
       return res.status(201).json(newUser)
     } catch (error: any) {
+
+      console.error('🔴 ERRO NO CADASTRO:', error)
+
       if (error instanceof z.ZodError) {
         return res.status(400).json({ errors: error.errors })
       }
       if (error.message === 'Este e-mail já está cadastrado.') {
         return res.status(409).json({ error: error.message })
       }
-
       return res.status(500).json({ error: 'Erro interno do servidor' })
     }
   },
